ec09deaf0d993c4a4457d40f3d7c9f1a4926d706
Morteim <lucia_GMR@hotmail.com>
2023-03-31 21:25:23 -0600

    Conexión PHP

---
+++ conexion.php
@@ -0,0 +1,20 @@
+<?php
+function connectDB(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "gestec";
+
+    // Crear una conexión
+    $conn = mysqli_connect($servername, $username, $password,$dbname);
+
+    // Verificar si hay errores en la conexión
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+echo "Conexión exitosa";
+?>
