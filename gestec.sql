1fd9f73fe0b8a89110c7059b527b28ea7ab658a8
Morteim <lucia_GMR@hotmail.com>
2023-05-06 18:31:21 -0600

    BD actualizada (se agregó una tabla para la pag. cambiar estatus)

--- gestec.sql
+++ gestec.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-03-2023 a las 04:58:19
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 07-05-2023 a las 02:30:07
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,7 +37,7 @@ CREATE TABLE `administrador` (
 --
 
 INSERT INTO `administrador` (`Correo`, `Contraseña`) VALUES
-('gestec@cucea.udg.mx', 'Pass321');
+('gestec@cucea.udg.mx', 'Pass4321');
 
 -- --------------------------------------------------------
 
@@ -46,9 +46,17 @@ INSERT INTO `administrador` (`Correo`, `Contraseña`) VALUES
 --
 
 CREATE TABLE `cuentas` (
+  `Codigo` varchar(9) NOT NULL,
   `Correo` varchar(45) NOT NULL,
   `Contraseña` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cuentas`
+--
+
+INSERT INTO `cuentas` (`Codigo`, `Correo`, `Contraseña`) VALUES
+('214603948', 'fatima@gmail.com', '12345678');
 
 -- --------------------------------------------------------
 
@@ -75,6 +83,25 @@ CREATE TABLE `equipos` (
   `Tipo` varchar(20) NOT NULL,
   `Modelo` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estatus_prestamos`
+--
+
+CREATE TABLE `estatus_prestamos` (
+  `Tipo_Prestamo` varchar(20) NOT NULL,
+  `Estatus` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estatus_prestamos`
+--
+
+INSERT INTO `estatus_prestamos` (`Tipo_Prestamo`, `Estatus`) VALUES
+('INTERNO', 'habilitado'),
+('EXTERNO', 'deshabilitado');
 
 -- --------------------------------------------------------
 
@@ -106,9 +133,15 @@ CREATE TABLE `usuarios` (
   `Carrera` varchar(45) NOT NULL,
   `Domicilio` varchar(45) NOT NULL,
   `Colonia` varchar(40) NOT NULL,
-  `Celular` varchar(10) NOT NULL,
-  `Correo` varchar(45) NOT NULL
+  `Celular` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`Codigo`, `Nombres`, `Ape_Pat`, `Ape_Mat`, `Carrera`, `Domicilio`, `Colonia`, `Celular`) VALUES
+('214603948', 'Fatima', 'Montañez', 'Orozco', 'Tecnologias', 'Santo Tomas 613', 'Mision San', '3315740445');
 
 --
 -- Índices para tablas volcadas
@@ -124,7 +157,7 @@ ALTER TABLE `administrador`
 -- Indices de la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-  ADD PRIMARY KEY (`Correo`);
+  ADD KEY `Codigo` (`Codigo`);
 
 --
 -- Indices de la tabla `documentos`
@@ -150,8 +183,7 @@ ALTER TABLE `prestamos`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`Codigo`),
-  ADD KEY `Correo` (`Correo`);
+  ADD PRIMARY KEY (`Codigo`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -168,18 +200,23 @@ ALTER TABLE `equipos`
 --
 
 --
+-- Filtros para la tabla `cuentas`
+--
+ALTER TABLE `cuentas`
+  ADD CONSTRAINT `cuentas_ibfk_1` FOREIGN KEY (`Codigo`) REFERENCES `usuarios` (`Codigo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  ADD CONSTRAINT `documentos_ibfk_1` FOREIGN KEY (`Codigo`) REFERENCES `usuarios` (`Codigo`) ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `prestamos`
 --
 ALTER TABLE `prestamos`
   ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `equipos` (`ID`) ON UPDATE CASCADE,
   ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`Codigo`) REFERENCES `usuarios` (`Codigo`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`Correo`) REFERENCES `cuentas` (`Correo`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`Codigo`) REFERENCES `documentos` (`Codigo`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
