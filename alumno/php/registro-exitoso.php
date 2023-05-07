c7439c420ccc252a3717fa6d843b0c98f5e593bd
Morteim <lucia_GMR@hotmail.com>
2023-05-06 20:11:50 -0600

    Pagina nueva para la conexion

---
+++ alumno/php/registro-exitoso.php
@@ -0,0 +1,8 @@
+<?php 
+    session_start();
+    session_unset();
+    session_destroy(); // Cierra la sesión
+    header('Location: index-usuario.html'); // Redirige al usuario a la página de inicio
+    exit();
+
+?>
