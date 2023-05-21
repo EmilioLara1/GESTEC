8f8807277edc44949bacc180ab5b94af94653f01
Morteim <lucia_GMR@hotmail.com>
2023-05-21 16:01:40 -0600

    Página nueva

--- alumno/html/solicitud-aceptada-externo.html.php
+++ alumno/html/solicitud-aceptada-externo.html.php
@@ -42,7 +42,38 @@
                             <p class="lead text-center folio">Folio</p>
                         </div>
                         <div class="row">
-                            <p class="lead text-center"> número de folio </p>
+                            <p class="lead text-center">
+                                <?php
+                                    require_once "conexion.php";
+
+                                    $conn = connectDB();
+
+                                    $folio = generarFolio();
+
+                                    $query = "SELECT Folio FROM prestamos WHERE Folio = '$folio'";
+                                    $resultado = $conn->query($query);
+
+                                    while (mysqli_num_rows($resultado) > 0) {
+                                        // Si el folio ya existe, generar uno nuevo
+                                        $folio = generarFolio();
+                                        $query = "SELECT Folio FROM prestamos WHERE Folio = '$folio'";
+                                        $resultado = $conn->query($query);
+                                    }
+
+                                    $query = "INSERT INTO prestamos (Folio) VALUES ('$folio')";
+                                    if ($conn->query($query) === TRUE) {
+                                        echo $folio;
+                                    } else {
+                                        echo "Error al insertar el folio en la base de datos: " . $conn->error;
+                                    }
+
+                                    $conn->close();
+
+                                    function generarFolio() {
+                                        return rand(10000, 99999);
+                                    }
+                                ?>
+                            </p>
                         </div>
                         <div class="row">
                             <hr>
