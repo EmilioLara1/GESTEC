1c9f70496c96a38a415ca99d42150967f7a0f944
Morteim <lucia_GMR@hotmail.com>
2023-05-20 20:02:46 -0600

    Página terminada

--- alumno/php/tipo-prestamo.php
+++ alumno/php/tipo-prestamo.php
@@ -9,11 +9,27 @@
 
     // Verifica qué opción se seleccionó y redirige a la página correspondiente
     if ($select == "interno") {
-        header("Location: pagina1.php");
-        exit();
+        $query = "SELECT * FROM estatus_prestamos WHERE Tipo_Prestamo = 'INTERNO' AND Estatus = 'habilitado'";
+        $resultado = mysqli_query($conn, $query);
+        
+        if (mysqli_num_rows($resultado) > 0) {
+            header("Location: solicitud-aceptada-interno.html");
+            exit();
+        } else {
+            header("Location: solicitud-rechazada.html");
+            exit();
+        }
     } else if ($select == "externo") {
-        header("Location: pagina2.php");
-        exit();
+        $query = "SELECT * FROM estatus_prestamos WHERE Tipo_Prestamo = 'EXTERNO' AND Estatus = 'habilitado'";
+        $resultado = mysqli_query($conn, $query);
+
+        if (mysqli_num_rows($resultado) > 0) {
+            header("Location: solicitud-aceptada-externo.html");
+            exit();
+        } else {
+            header("Location: convocatoria-cerrada.html");
+            exit();
+        }
     } else {
         // Si no se seleccionó ninguna opción, redirige a una página de error
         echo "No seleccionaste ninguna opción.";
