102f1d4bcdad373471d9156214be72c95fc86e97
Morteim <lucia_GMR@hotmail.com>
2023-05-22 01:46:38 -0600

    Final

--- administrador/php/registrar-prestamo-gestec.php
+++ administrador/php/registrar-prestamo-gestec.php
@@ -14,26 +14,7 @@
 
         $queryID = "UPDATE prestamos SET ID = '$id' WHERE Folio = '$folio'";
         $resultadoInsercion = mysqli_query($conn, $queryID);
-
-        // Obtener la fecha de entrega actual
-        $consultaFechaPrestamo = "SELECT Fecha_Prestamo FROM prestamos WHERE ID = $id AND folio = '$folio'";
-        $resultadoFechaPrestamo = mysqli_query($conn, $consultaFechaPrestamo);
-        $filaFechaPrestamo = mysqli_fetch_assoc($resultadoFechaPrestamo);
-        $fechaFechaPrestamo = $filaFechaPrestamo['Fecha_Prestamo'];
-
-        // Calcular la fecha de devolución (3 horas después)
-        $fechaDevolucion = date('Y-m-d H:i:s', strtotime($fechaFechaPrestamo . ' + 3 hours'));
-
-        // Actualizar la fecha de devolución en la tabla
-        $consulta = "UPDATE prestamos SET Fecha_Devolucion = '$fechaDevolucion'";
-
-        if (mysqli_query($conn, $consulta)) {
-            echo "Fecha de devolución actualizada exitosamente.";
-          } else {
-            echo "Error al actualizar la fecha de devolución: " . mysqli_error($conn);
-          }
         
-        // Ingresar estatus
         $query = "SELECT Fecha_Prestamo, Fecha_Devolucion FROM prestamos WHERE ID = $id AND folio = '$folio'";
         $resultado = mysqli_query($conn, $query);
 
@@ -47,15 +28,11 @@
         $fechaDevolucionObj = new DateTime($fechaDevolucion);
 
         // Calcular la diferencia en días entre las fechas
-        $diferenciaDias = $fechaPrestamoObj->diff($fechaDevolucionObj);
-
-        // Obtener la diferencia en días y horas
-        $dias = $diferencia->days;
-        $horas = $diferencia->h;
+        $diferenciaDias = $fechaPrestamoObj->diff($fechaDevolucionObj)->days;
 
         // Determinar el estado del préstamo
-        if ($dias > 0 || $horas > 0) {
-            $estado = "RETRASADO";
+        if ($diferenciaDias > 0) {
+            $estatus = "RETRASADO";
             $query = "UPDATE prestamos SET Estatus = '$estatus' WHERE Folio = '$folio'";
             if ($conn->query($query) === TRUE) {
                 header("Location: prestamo-denegado.html");
@@ -64,7 +41,7 @@
                 echo "Ha ocurrido un error al registrar el estatus en la base de datos";
             }
         } else {
-            $estado = "ACTIVO";
+            $estatus = "ACTIVO";
             $query = "UPDATE prestamos SET Estatus = '$estatus' WHERE Folio = '$folio'";
             if ($conn->query($query) === TRUE) {
                 header("Location: prestamo-resgistrado.html");
@@ -73,7 +50,6 @@
                 echo "Ha ocurrido un error al registrar el estatus en la base de datos";
             }
         }
-
 
         // Liberar memoria y cerrar la conexión
         mysqli_free_result($resultado);
