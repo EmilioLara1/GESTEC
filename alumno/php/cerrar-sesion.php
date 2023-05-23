1785d55e698f5aa81a4959daca05dce614620ff0
EmilioLara1 <emiliolarap@gmail.com>
2023-05-22 20:16:32 -0600

    Refresh usuario

--- alumno/php/cerrar-sesion.php
+++ alumno/php/cerrar-sesion.php
@@ -2,6 +2,6 @@
     session_start();
     session_unset();
     session_destroy(); // Cierra la sesión
-    header('Location: index-usuario.html'); // Redirige al usuario a la página de inicio
+    header('Location: index-usuario.html; Refresh:0'); // Redirige al usuario a la página de inicio
     exit();
 ?>
